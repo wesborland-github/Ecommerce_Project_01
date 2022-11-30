6578b9bfad641c048a199ff7df2399519c7df65a
Marcos Javier Gómez Höllger <marcosjaviergomezh@gmail.com>
2022-11-30 09:42:45 -0300

    Project ecommerce v01
    Ecommerce v01 with:
    - Header with Icon, Search bar and cart icon: OK.
    - Search bar Function : OK.
    - Api with products fake : OK.
    - Put all products of api in cards: OK.
    - Add products to cart : OK.
    - Add filter category for products : OK.
    - Checkout cart: OK.
    - Function delete cart product : OK.
    - Function continue shopping: OK.
    - Function delete all cart products: OK.
    - Function total checkout cart: OK.
    - Add count of products added to cart icon dinamically: OK.

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FilterPipe } from './shared/filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    FilterPipe,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
